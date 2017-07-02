915aaef863058fb8b3eb7122cade9e1ff2746d18
timeloveboy <734991033@qq.com>
2017-07-02 23:39:41 +0800

    ~

--- python2.7/install.sh
+++ python2.7/install.sh
@@ -3,7 +3,7 @@ pyversion="2.7.13"
 apt-get install -y  --fix-missing gcc make
 wget "https://www.python.org/ftp/python/${pyversion}/Python-${pyversion}.tgz"  --progress=bar:force 2>&1
 tar xzvf Python-${pyversion}.tgz
-rm -r Python-2.7.13.tgz
+rm -r Python-${pyversion}.tgz
 mv Python-${pyversion} /opt/Python-${pyversion}
 cd /opt/Python-${pyversion}
 pwd
