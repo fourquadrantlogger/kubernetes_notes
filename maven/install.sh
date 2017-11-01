8e15bdd076ad37cf25988b92eef887f8a113134c
timeloveboy <734991033@qq.com>
2017-11-01 14:47:02 +0800

    ~

--- maven/install.sh
+++ maven/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~
-wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz --progress=bar:force 2>&1
+wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz --progress=bar:force 2>&1
 tar xzvf apache-maven-3.3.9-bin.tar.gz
 mv apache-maven-3.3.9  /opt/maven
 rm apache-maven-3.3.9-bin.tar.gz
